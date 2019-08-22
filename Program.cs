64753a8945733d076d96c3d8c72772248e9def1c
lucianoterziotti <lucianoterziotti@outlook.com>
2019-08-22 15:39:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace veterinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
